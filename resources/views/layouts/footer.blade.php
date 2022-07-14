7a159ac81127211819b38bff2558d204a9ca80b1
Wakil18 <wakilahmed18@gmail.com>
2022-07-14 10:49:11 +0600

    Added footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,12 @@
+<footer class="gradient mt-3">
+    <div class="container-fluid text-center" >
+        <span>Made with <3 by
+            <a href="">Wakil Ahmed</a>
+        </span>
+    </div>
+</footer>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
